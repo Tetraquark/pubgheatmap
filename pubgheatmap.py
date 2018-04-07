0ded12b8e256b0699e3a04d5faf575baf0240f9d
Tetraquark <avladislavv@gmail.com>
2018-04-07 21:00:00 +0700

    added comments and some fixes

--- pubgheatmap.py
+++ pubgheatmap.py
@@ -9,12 +9,12 @@ MIRAMAR_MAP_IMG_PATH = 'data/img/miramar_map.jpg'
 
 MAPS_IMGS_PATHS = {'Desert_Main' : MIRAMAR_MAP_IMG_PATH, 'Erangel_Main' : ERANGEL_MAP_IMG_PATH}
 
-def buildHeatMap(pointsList, imgFile_path, outImgPath):
+def buildHeatMap(pointsList, imgFile_path):
     mapimg = Image.open(imgFile_path)
 
     heatmapper = Heatmapper(point_diameter=25, point_strength=0.2, opacity=0.7)
-    heatmap = heatmapper.heatmap_on_img(pointsList, mapimg)
-    heatmap.save(outImgPath)
+    heatmapImg = heatmapper.heatmap_on_img(pointsList, mapimg)
+    return heatmapImg
 
 def getTelemetryPlayersCoords(telemetry):
     player_positions_events = telemetry.events_from_type('LogPlayerPosition')
@@ -32,18 +32,13 @@ def getTelemetryMapName(telemetry):
     events = telemetry.events_from_type('LogPlayerPosition')
     return events[0].common.map_name
 
-if __name__ == "__main__":
-    player_name = 'tetraquark'
-    match_number = 0
-    out_heatmap_file_name = 'pubgheatmap11.png'
+def getMatchHeatmap(match):
+    """
+    Make a heatmap of players activity of the match.
 
-    api = PUBG(API_KEY, Shard.PC_EU)
-    players = api.players().filter(player_names=[player_name])
-    myPlayer = players[0]
-
-    mathcesIdList = [match.id for match in myPlayer.matches]
-    match = api.matches().get(mathcesIdList[match_number])
-
+    :param match: pubg_python.match object
+    :return: PIL Image
+    """
     asset = match.assets[0]
     telemetry = api.telemetry(asset.url)
 
@@ -51,6 +46,28 @@ if __name__ == "__main__":
     mapImgPath = MAPS_IMGS_PATHS[mapName]
 
     playersCoords = getTelemetryPlayersCoords(telemetry)
+    heatmapImg = buildHeatMap(playersCoords, mapImgPath)
 
+    return heatmapImg
 
-    buildHeatMap(playersCoords, MIRAMAR_MAP_IMG_PATH, out_heatmap_file_name)
+if __name__ == "__main__":
+    player_name = 'tetraquark'
+    match_number = 0
+    out_heatmap_file_name = 'pubgheatmap11.jpg'
+    server = Shard.PC_EU
+
+    api = PUBG(API_KEY, server)
+
+    # get required player
+    players = api.players().filter(player_names=[player_name])
+    myPlayer = players[0]
+
+    # get the last player matches
+    mathcesIdList = [match.id for match in myPlayer.matches]
+    # get required match object
+    match = api.matches().get(mathcesIdList[match_number])
+
+    # get match heatmap (PIL image file)
+    heatmapImg = getMatchHeatmap(match=match)
+    # save image to the file
+    heatmapImg.save(out_heatmap_file_name)
